8ded229b81e45ff68ff19c58b4bc4813a5edb1b0
Nastya <realovka@mail.ru>
2020-05-19 11:38:09 +0300

    2 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] array = new int[3][3];
+        char[][] array = new char[3][3];
         boolean a = true;
 
         int i, j, x, y;
         // выводим пустой массив
         for (i = 0; i < array.length; i++) {
             for (j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+                System.out.print(array[i][j]='_');
+                System.out.print(" ");
             }
             System.out.println();
         }
 
-        int player = 1;
+        char player = 'O';
 
 
         int f = 0;
         while (a && f != 9) {
 
-            System.out.println("Игрок номер " + player + " введите номер ячейки по горизонтали, куда хотите поставить свою фигуру 0..2");
+            System.out.println("Игрок номер " + ((player>>4)-3) + " введите номер ячейки по горизонтали, куда хотите поставить свою фигуру 0..2");
             Scanner scanner = new Scanner(System.in);
             x = scanner.nextInt();
-            System.out.println("Игрок номер " + player + " введите номер ячейки по вертикали, куда хотите поставить свою фигуру 0..2");
+            System.out.println("Игрок номер " + ((player>>4)-3) + " введите номер ячейки по вертикали, куда хотите поставить свою фигуру 0..2");
             Scanner scanner1 = new Scanner(System.in);
             y = scanner1.nextInt();
             for (i = 0; i <= 2; i++) {
                 for (j = 0; j <= 2; j++) {
-                    if (array[x][y] == 0) {       //если ячейка, куда хочет поставить знак  игрок свободна , то поставить в нее знак и вывести массив
-                        array[x][y] = player;
+                    if (array[x][y] == 95 ) {       //если ячейка, куда хочет поставить знак  игрок свободна , то поставить в нее знак и вывести массив
+                        array[x][y] =  player;
                         f++;
                         for (i = 0; i <= 2; i++) {
                             for (j = 0; j <= 2; j++) {
@@ -40,22 +41,22 @@ public class Main {
                             }
                             System.out.println();
                         }
-                        if (f != 9 && (array[0][0] != 0 && array[0][0] == array[1][1] && array[1][1] == array[2][2]) ||
-                                (array[0][0] != 0 && array[0][0] == array[0][1] && array[0][1] == array[0][2]) ||
-                                (array[0][0] != 0 && array[0][0] == array[1][0] && array[1][0] == array[2][0]) ||
-                                (array[1][0] != 0 && array[1][0] == array[1][1] && array[1][1] == array[1][2]) ||
-                                (array[2][0] != 0 && array[2][0] == array[2][1] && array[2][1] == array[2][2]) ||
-                                (array[0][1] != 0 && array[0][1] == array[1][1] && array[1][1] == array[2][1]) ||
-                                (array[0][2] != 0 && array[0][2] == array[1][2] && array[1][2] == array[2][2]) ||
-                                (array[2][0] != 0 && array[2][0] == array[1][1] && array[1][1] == array[0][2])) {
-                            System.out.println("Игрок номер " + player + " выиграл");
+                        if (   f != 9 && (array[0][0] != 95 && array[0][0] == array[1][1] && array[1][1] == array[2][2]) ||
+                                (array[0][0] != 95 && array[0][0] == array[0][1] && array[0][1] == array[0][2]) ||
+                                (array[0][0] != 95 && array[0][0] == array[1][0] && array[1][0] == array[2][0]) ||
+                                (array[1][0] != 95 && array[1][0] == array[1][1] && array[1][1] == array[1][2]) ||
+                                (array[2][0] != 95 && array[2][0] == array[2][1] && array[2][1] == array[2][2]) ||
+                                (array[0][1] != 95 && array[0][1] == array[1][1] && array[1][1] == array[2][1]) ||
+                                (array[0][2] != 95 && array[0][2] == array[1][2] && array[1][2] == array[2][2]) ||
+                                (array[2][0] != 95 && array[2][0] == array[1][1] && array[1][1] == array[0][2])) {
+                            System.out.println("Игрок номер " + ((player>>4)-3) + " выиграл");
                             a = false;
                             break;
                         } else if (f != 9) {
-                            if (player == 1) {
-                                player = 2;
+                            if (player == 'O') {
+                                player = 'X';
                             } else {
-                                player = 1;
+                                player = 'O';
                             }
                         } else {
                             System.out.println("Ничья. Игра окончена");
